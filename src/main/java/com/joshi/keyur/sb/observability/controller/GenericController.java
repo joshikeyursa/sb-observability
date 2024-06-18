fb9c5e50cfe0612cdecc1371b8d608405703eaea
keyurjoshi <joshikeyursa@gmail.com>
2024-06-18 23:39:11 +1000

    Added sb profile yaml files for running multiple instances in ||

--- src/main/java/com/joshi/keyur/sb/observability/controller/GenericController.java
+++ src/main/java/com/joshi/keyur/sb/observability/controller/GenericController.java
@@ -2,6 +2,7 @@ package com.joshi.keyur.sb.observability.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,9 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @RequestMapping("/api")
 public class GenericController {
+
+    @Value("${superhero}")
+    private String superHero;
     private final RestTemplate restTemplate;
     private static final Logger logger = LoggerFactory.getLogger(GenericController.class);
     public GenericController(RestTemplate restTemplate){
@@ -19,13 +23,17 @@ public class GenericController {
     @GetMapping("/sayHi")
     public ResponseEntity<String> sayHi(){
         logger.info("Will say Hi");
+        String portNumber = "8095";
+        if(Math.random()<0.5){
+            portNumber = "8096";
+        }
         String name = restTemplate
-                .getForObject("http://localhost:8090/api/fetchUser",String.class);
+                .getForObject("http://localhost:"+portNumber+"/api/fetchUser",String.class);
         return ResponseEntity.ok("Hi, "+name);
     }
     @GetMapping("/fetchUser")
     public ResponseEntity<String> fetchUser(){
-        logger.info("Returning Spiderman!");
-        return ResponseEntity.ok("Spiderman!");
+        logger.info("Returning {}!",superHero);
+        return ResponseEntity.ok(superHero);
     }
 }
