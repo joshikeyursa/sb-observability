db6401924cfdf61ce34a96018e6b7ad0f5ab82b3
keyurjoshi <joshikeyursa@gmail.com>
2024-06-18 23:03:22 +1000

    Basic stuff added. Yet to start with Observability

--- src/main/java/com/joshi/keyur/sb/observability/controller/GenericController.java
+++ src/main/java/com/joshi/keyur/sb/observability/controller/GenericController.java
@@ -1,5 +1,8 @@
 package com.joshi.keyur.sb.observability.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,19 +11,21 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @RequestMapping("/api")
 public class GenericController {
-    RestTemplate restTemplate;
-    GenericController(RestTemplate restTemplate){
+    private final RestTemplate restTemplate;
+    private static final Logger logger = LoggerFactory.getLogger(GenericController.class);
+    public GenericController(RestTemplate restTemplate){
         this.restTemplate = restTemplate;
     }
     @GetMapping("/sayHi")
-    public String sayHi(){
-        String user = restTemplate
-                .getForEntity("http://localhost:8080/api/fetchUser",String.class)
-                .getBody();
-        return "Hi, "+user;
+    public ResponseEntity<String> sayHi(){
+        logger.info("Will say Hi");
+        String name = restTemplate
+                .getForObject("http://localhost:8090/api/fetchUser",String.class);
+        return ResponseEntity.ok("Hi, "+name);
     }
     @GetMapping("/fetchUser")
-    public String fetchUser(){
-        return "Spiderman";
+    public ResponseEntity<String> fetchUser(){
+        logger.info("Returning Spiderman!");
+        return ResponseEntity.ok("Spiderman");
     }
 }
